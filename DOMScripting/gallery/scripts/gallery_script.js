60987056ad2886694c23792e4cb41b43047fdb9e
Thomas Michael <31713144+michaelmit@users.noreply.github.com>
2017-12-26 15:40:23 +0400

    finished script

--- DOMScripting/gallery/scripts/gallery_script.js
+++ DOMScripting/gallery/scripts/gallery_script.js
@@ -1,5 +1,26 @@
 // if (!document.getElementsByTagName) return false; 
 
+// select pic
+function showpic(whichpic) {
+    if (document.getElementById("placeholder"))  {
+        var source = whichpic.getAttribute("href");
+        var placeholder = document.getElementById("placeholder")
+        placeholder.setAttribute("src", source); // swap link
+
+        // set description
+        if (document.getElementById("description")) {
+            var title = whichpic.getAttribute("title"); 
+            var description = document.getElementById("description");
+            if (description.firstChild.nodeType == 3){ // textNode 
+                description.firstChild.nodeValue = title; // set description
+            }
+            return true; // swap successful
+        } 
+    }
+    return false; // swap unsuccessful
+}
+
+// show image in gallery with correct title
 function prepareGallery() {
 
     // check browser support
@@ -12,11 +33,12 @@ function prepareGallery() {
 
     // get all links in DOM id=image_gallery
     var links = gallery.getElementsByTagName("a");
-    // console.log(links);
     // loop though all links and attach behavior to view in gallery
     for (var i = 0; i < links.length; ++i) {
         links[i].onclick = function() {
-            
+            return showpic(this) ? false : true; 
+            // if swap successful, showpic returns true
+            // return value of event handler determines whether default behavior takes place
         };
     }
 }
