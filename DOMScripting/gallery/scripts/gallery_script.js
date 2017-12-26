74dc5cc6cf510176212a34c59dfe75c99cf4bb9f
Thomas Michael <31713144+michaelmit@users.noreply.github.com>
2017-12-27 03:46:07 +0400

    dynamic markup

--- DOMScripting/gallery/scripts/gallery_script.js
+++ DOMScripting/gallery/scripts/gallery_script.js
@@ -20,6 +20,80 @@ function showpic(whichpic) {
     return false; // swap unsuccessful
 }
 
+// placeholder to place the gallery and description
+// inserts into document instead of hardcoding in the HTML
+function preparePlaceHolder() {
+
+    if (!document.createTextNode ||
+        !document.createElement ||
+        !document.getElementById ||
+        !document.getElementById("image_gallery")) 
+    { 
+        return false;
+    }
+
+    /* create image placeholder for gallery in DOM 
+     * <img id="placeholder" src="images/placeholder.gif" alt="my image gallery" /> 
+     * <p id="description"> Select an image </p> 
+     */
+    var placeholder = document.createElement("img"); 
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+    // paragraph text description
+    description = document.createElement("p");
+    description.setAttribute("id", "description");
+    desctext = document.createTextNode("Select an image"); 
+    // add description textNode to paragraph Node
+    placeholder.appendChild(desctext);
+
+   
+    // Add above placeholder element to the DOM 
+    // insert placeholder in the DOM, right after the image_gallery 
+    var target = getElementById("image_gallery");
+    insertAfter(placeholder, gallery); // this is user defined
+
+} 
+
+// inserts source element after target element(DOM methods only provide insertBefore)
+function insertAfter(source, target) {
+    
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+                
+
+
+
+    // var image_placeholder = document.createElement("img");
+    // // set id, src and title for the new image
+    // image_placeholder.setAttribute("id", "placeholder");
+    // image_placeholder.setAttribute("src", "images/placeholder.gif");
+    // image_placeholder.setAttribute("alt", "My Image Gallery");
+
+    // // create element for desciption text
+    // var para = document.createElement("p");
+    // // set id for para
+    // para.setAttribute("id", "description on image")    
+    // // create text element and append to para element
+    // var desctext = createTextNode("click on image");
+    // para.appendChild(desctext);
+
+    // image_placeholder.appendChild(para);
+
+    // // append newly created nodes into the DOM
+    // // insert image placeholder after gallery
+    // var gallery = getElementById("image_gallery"); // unordered list, mini gallery
+    // insertAfter(gallery, image_placeholder); // insertAfter(target, newElement);
+
+
 // show image in gallery with correct title
 function prepareGallery() {
 
