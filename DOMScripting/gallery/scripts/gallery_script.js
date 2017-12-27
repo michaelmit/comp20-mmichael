785bfb462eb334beef42b5889ca04b2ae4821836
Thomas Michael <31713144+michaelmit@users.noreply.github.com>
2017-12-27 05:20:46 +0400

    added markup on the fly

--- DOMScripting/gallery/scripts/gallery_script.js
+++ DOMScripting/gallery/scripts/gallery_script.js
@@ -2,10 +2,15 @@
 
 // select pic
 function showpic(whichpic) {
+
+    // if (!document.getElementById("placeholder") )
+    //     return false;
+
     if (document.getElementById("placeholder"))  {
         var source = whichpic.getAttribute("href");
         var placeholder = document.getElementById("placeholder")
         placeholder.setAttribute("src", source); // swap link
+
 
         // set description
         if (document.getElementById("description")) {
@@ -22,7 +27,7 @@ function showpic(whichpic) {
 
 // placeholder to place the gallery and description
 // inserts into document instead of hardcoding in the HTML
-function preparePlaceHolder() {
+function preparePlaceholder() {
 
     if (!document.createTextNode ||
         !document.createElement ||
@@ -41,57 +46,35 @@ function preparePlaceHolder() {
     placeholder.setAttribute("src", "images/placeholder.gif");
     placeholder.setAttribute("alt", "my image gallery");
     // paragraph text description
-    description = document.createElement("p");
+    var description = document.createElement("p");
     description.setAttribute("id", "description");
     desctext = document.createTextNode("Select an image"); 
     // add description textNode to paragraph Node
-    placeholder.appendChild(desctext);
+    description.appendChild(desctext);
 
    
     // Add above placeholder element to the DOM 
     // insert placeholder in the DOM, right after the image_gallery 
-    var target = getElementById("image_gallery");
-    insertAfter(placeholder, gallery); // this is user defined
+    var target = document.getElementById("image_gallery");
+    // this is user defined insertAfter(src, target)
+    insertAfter(placeholder, target); 
+    insertAfter(description, placeholder); 
 
 } 
 
 // inserts source element after target element(DOM methods only provide insertBefore)
 function insertAfter(source, target) {
-    
+    // insert after target (check whether target is last child, otherwise insert before next sibling)    
+    var parent = target.parentNode; // parent is body
+    if (parent.lastElementChild == target) {
+        parent.appendChild(source); // append as child to body 
+    }
+    else {
+        target.insertBefore(source, target.nextSibling);
+    }
 }
    
    
-   
-   
-   
-   
-   
-   
-   
-                
-
-
-
-    // var image_placeholder = document.createElement("img");
-    // // set id, src and title for the new image
-    // image_placeholder.setAttribute("id", "placeholder");
-    // image_placeholder.setAttribute("src", "images/placeholder.gif");
-    // image_placeholder.setAttribute("alt", "My Image Gallery");
-
-    // // create element for desciption text
-    // var para = document.createElement("p");
-    // // set id for para
-    // para.setAttribute("id", "description on image")    
-    // // create text element and append to para element
-    // var desctext = createTextNode("click on image");
-    // para.appendChild(desctext);
-
-    // image_placeholder.appendChild(para);
-
-    // // append newly created nodes into the DOM
-    // // insert image placeholder after gallery
-    // var gallery = getElementById("image_gallery"); // unordered list, mini gallery
-    // insertAfter(gallery, image_placeholder); // insertAfter(target, newElement);
 
 
 // show image in gallery with correct title
@@ -117,8 +100,28 @@ function prepareGallery() {
     }
 }
 
-window.onload = prepareGallery; 
+// on window load, call prepareGallery, preparePlaceholder
+function addLoadEvent(func) {
+    // if existing onload is a function, add to it Otherwise link new function
+    prevOnload = window.onload;
+    if (typeof window.onload != 'function') {
 
+        window.onload = func;
+    }
+    else {
+        // call all functions
+        window.onload = function () {
+        // var elems = document.body.getElementsByTagName("*");
+        prevOnload();
+        func();
+        };
+    }
+}
+
+
+// call load events
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
 
 
 
